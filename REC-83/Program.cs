44de12641dad418e29a511ebdf4f9f86bc0ff24c
сергей токарев <sseregatokarew@gmail.com>
2021-12-11 18:14:18 +0700

    Добавьте файлы проекта.

---
+++ REC-83/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace REC_83
+{
+    class Program
+    {
+        static long Pow(long x, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            if (n % 2 == 0)
+            {
+                var result = Pow(x, n / 2);
+                return result * result;
+            }
+            else
+            {
+                return x * Pow(x, n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите основание: ");
+            var x = long.Parse( Console.ReadLine());
+            Console.Write("Введите степень: ");
+            var n = int.Parse(Console.ReadLine());
+            Console.Write(Pow(x, n));
+            Console.ReadKey();
+        }
+    }
+}
